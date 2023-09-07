6c42937c9c1df928b16243bdb138f1d865b2c8e9
Mateus Anjos <mateus.anjos@aarin.com.br>
2023-09-07 20:11:43 -0300

    fix issues: conflict add new commands and agnostic package

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
-const fs = require("fs");
+#!/usr/bin/env node
+
+const fs = require("fs").promises;
 const path = require("path");
+const { Command } = require("commander");
 
-const readFileSync = fs.readFileSync;
-const writeFileSync = fs.writeFileSync;
-const readdirSync = fs.readdirSync;
-const join = path.join;
+const program = new Command();
+program.version("1.0.0");
 
+program
+  .option("-d, --directory <directory>", "Directory to process")
+  .option("-e, --extensions <extensions>", "File extensions to process (comma-separated)")
+  .option("-x, --excludeFolders <excludeFolders>", "Folders to exclude (comma-separated)")
+  .option("-h, --help", "ϟ Check documentation: https://github.com/omateusanjos/css-absolute-values-to-px")
+  .parse(process.argv);
 
-function convertToPx(value) {
-  if (/^\d+$/.test(value)) {
-    return `"${value}px"`
-  }
-  return value;
-}
-
-function processFile(filePath) {
+async function processFile(filePath) {
   try {
-    let fileContent = readFileSync(filePath, "utf8");
+    const fileContent = await fs.readFile(filePath, "utf8");
     const convertedContent = fileContent.replace(
       /(\b\d+\b)(?![\s\S]*["'])/g,
-      (_, value) => convertToPx(value)
+      (_, value) => `"${value}px"`
     );
 
-    writeFileSync(filePath, convertedContent);
+    await fs.writeFile(filePath, convertedContent);
     console.log(`File update: ${filePath}`);
   } catch (error) {
-    console.error(`Error to convert ${filePath}: ${error}`);
+    console.error(`Error converting ${filePath}: ${error}`);
   }
 }
 
-const rootDirectory = "./";
+async function processDirectory(directoryPath, extensions, excludedFolders = []) {
+  try {
+    const files = await fs.readdir(directoryPath);
 
-readdirSync(rootDirectory).forEach((file) => {
-  const filePath = join(rootDirectory, file);
-  const extensionsToProcess = [".ts", ".tsx", ".js", ".jsx", ".css"];
+    for (const file of files) {
+      const filePath = path.join(directoryPath, file);
+      const stat = await fs.stat(filePath);
 
-  if (extensionsToProcess.some((ext) => file.endsWith(ext))) {
-    processFile(filePath);
+      if (stat.isDirectory() && !excludedFolders.includes(file)) {
+        await processDirectory(filePath, extensions, excludedFolders);
+      } else if (extensions.some((ext) => file.endsWith(ext))) {
+        await processFile(filePath);
+      }
+    }
+  } catch (error) {
+    console.error(`Error processing directory ${directoryPath}: ${error}`);
   }
-});
+}
+
+async function main() {
+  const options = program.opts();
+
+  if (options.help) {
+    program.outputHelp();
+    process.exit(0);
+  }
+
+  const { directory, extensions, excludeFolders = [] } = options;
+
+  if (!directory || !extensions) {
+    console.error("Usage: my-script -d <directory> -e <extensions> [-x <excludeFolders>] [-h]");
+    process.exit(1);
+  }
+
+  await processDirectory(directory, extensions.split(","), excludeFolders.split(","));
+}
+
+main();
