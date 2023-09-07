48315e5e8955a19464d1b521a5b2506110589997
Mateus Anjos <mateus.anjos@aarin.com.br>
2023-09-07 20:24:02 -0300

    fragmented packages insert filter only . or ;

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-const fs = require("fs").promises;
-const path = require("path");
 const { Command } = require("commander");
+const processDirectory = require("./processDirectory");
 
 const program = new Command();
 program.version("1.0.0");
@@ -13,40 +12,6 @@ program
   .option("-x, --excludeFolders <excludeFolders>", "Folders to exclude (comma-separated)")
   .option("-h, --help", "ϟ Check documentation: https://github.com/omateusanjos/css-absolute-values-to-px")
   .parse(process.argv);
-
-async function processFile(filePath) {
-  try {
-    const fileContent = await fs.readFile(filePath, "utf8");
-    const convertedContent = fileContent.replace(
-      /(\b\d+\b)(?![\s\S]*["'])/g,
-      (_, value) => `"${value}px"`
-    );
-
-    await fs.writeFile(filePath, convertedContent);
-    console.log(`File update: ${filePath}`);
-  } catch (error) {
-    console.error(`Error converting ${filePath}: ${error}`);
-  }
-}
-
-async function processDirectory(directoryPath, extensions, excludedFolders = []) {
-  try {
-    const files = await fs.readdir(directoryPath);
-
-    for (const file of files) {
-      const filePath = path.join(directoryPath, file);
-      const stat = await fs.stat(filePath);
-
-      if (stat.isDirectory() && !excludedFolders.includes(file)) {
-        await processDirectory(filePath, extensions, excludedFolders);
-      } else if (extensions.some((ext) => file.endsWith(ext))) {
-        await processFile(filePath);
-      }
-    }
-  } catch (error) {
-    console.error(`Error processing directory ${directoryPath}: ${error}`);
-  }
-}
 
 async function main() {
   const options = program.opts();
---
+++ processDirectory.js
@@ -0,0 +1,38 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+async function processFile(filePath) {
+  try {
+    const fileContent = await fs.readFile(filePath, "utf8");
+    const convertedContent = fileContent.replace(
+      /(\b\d+\b)(?![\s\S]*["'])(?=[,;])/g,
+      (_, value) => `"${value}px"`
+    );
+
+    await fs.writeFile(filePath, convertedContent);
+    console.log(`File update: ${filePath}`);
+  } catch (error) {
+    console.error(`Error converting ${filePath}: ${error}`);
+  }
+}
+
+async function processDirectory(directoryPath, extensions, excludedFolders = []) {
+  try {
+    const files = await fs.readdir(directoryPath);
+
+    for (const file of files) {
+      const filePath = path.join(directoryPath, file);
+      const stat = await fs.stat(filePath);
+
+      if (stat.isDirectory() && !excludedFolders.includes(file)) {
+        await processDirectory(filePath, extensions, excludedFolders);
+      } else if (extensions.some((ext) => file.endsWith(ext))) {
+        await processFile(filePath);
+      }
+    }
+  } catch (error) {
+    console.error(`Error processing directory ${directoryPath}: ${error}`);
+  }
+}
+
+module.exports = processDirectory;
